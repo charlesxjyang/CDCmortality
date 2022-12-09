3b0c0d5213c53a2192731375d1e4323d4cf01846
Charles Yang <charlesxjyang@gmail.com>
2022-12-08 23:06:50 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -14,9 +14,9 @@ df = pd.read_pickle("data/cleaned_dataframe.pkl")
 st.title("CDC Leading Cause's of Death Data and Cars")
 
 intro_text = '''
-The CDC provides data on the leading causes of death for Americans every year by age group. The figure shown below is snipped version from the CDC website. Immediately you notice that a very large category is "Unintentional Injury", which is the most common cause of death for ages 1-44. 
+The [CDC provides data on the leading causes of death for Americans every year by age group](https://wisqars.cdc.gov/data/lcd/home). The figure shown below is a snipped version from the CDC website. Immediately you notice that a very large category is `Unintentional Injury`, which is the most common cause of death for ages 1-44. 
 
-What exactly is "Unintentional Injury"? Well, its a host of many different types of causes, but the most common is **Motor Vehicle's**. What would this plot look like if we separated out **Motor Vehicle's** as their own cause of death?
+What exactly constitutes an `Unintentional Injury`? Well, it serves as a bucket category for many different types of causes, but the most common cause of death contained within `Unintentional Injury` is `Motor vehicle, traffic`, that is, deaths that result from being in a car or hit by a car. What would this plot look like if we separated out `Motor Vehicles` as their own cause of death?
 '''
 
 st.markdown(intro_text)
@@ -29,7 +29,7 @@ st.image(
 )
 
 transition_text = '''
-We pull the [CDC's own data](https://wonder.cdc.gov/controller/saved/D76/D316F097) to recreate the above plot. The only difference is that we separate out Motor Vehicle's from "Unintentional Injury". Small discrepancies exist due to updates to the dataset and discrepancies in coding.
+I pulled the [CDC's own data](https://wonder.cdc.gov/controller/saved/D76/D316F097) to recreate the above plot. The only difference is that we separate out `Motor Vehicles` from `Unintentional Injury`. Small discrepancies exist due to updates to the dataset and discrepancies in coding.
 '''
 
 st.markdown(transition_text)
@@ -38,6 +38,13 @@ fig = create_heatmap(df)
 st.write(fig)
 
 # Create the dropdown menu
+
+bar_chart_text = '''
+You can also plot the number of deaths from each cause by age group here below:
+'''
+
+st.markdown(bar_chart_text)
+
 selected_age = st.selectbox("Select Age Range", display_ages)
 
 data = df[df['Ten-Year Age Groups Code'] == selected_age][[
